a740014f5bccb4accf830346d70f9c5dc6788351
Max Liashuk <m_lyashuk@ukr.net>
2023-08-24 23:44:13 -0400

    refactor: simplify rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,27 +3,25 @@
 import typescript from '@rollup/plugin-typescript'
 import json from '@rollup/plugin-json'
 
-export default [
-  {
-    input: 'src/index.ts',
-    external: ['vitest'],
-    output: [
-      {
-        file: 'lib/index.esm.js',
-        format: 'esm',
-        sourcemap: true,
-      },
-      {
-        file: 'lib/index.cjs.js',
-        format: 'cjs',
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      typescript({
-        tsconfig: './tsconfig.base.json',
-      }),
-      json(),
-    ],
-  },
-]
+export default {
+  input: 'src/index.ts',
+  external: ['vitest'],
+  output: [
+    {
+      file: 'lib/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+    {
+      file: 'lib/index.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.base.json',
+    }),
+    json(),
+  ],
+}
